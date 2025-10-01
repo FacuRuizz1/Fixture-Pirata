d31efe981a8b492e57d34b49d143cb4a7b74a221
114318-Ruiz, Facundo Nicolas <114318@tecnicatura.frc.utn.edu.ar>
2025-10-01 20:48:08 -0300

    creacion proyecto

---
+++ Backend/fixture-pirata/src/main/java/com/fixturepirata/FixturePirataApplication.java
@@ -0,0 +1,13 @@
+package com.fixturepirata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixturePirataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixturePirataApplication.class, args);
+    }
+
+}
